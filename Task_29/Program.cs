68b6fbdad4c623f662f4ed2f156ab8375f43c22b
Timerkhan <timerkhan12@gmail.com>
2022-12-15 22:41:46 +0500

    done

---
+++ Task_29/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] num = new int[8];
+Console.WriteLine("Введите цифры в соответствии с требуемым входом (цифра -> запята -> пробел):");
+string[] arr = Console.ReadLine().Split(", ");
+System.Console.Write("[");
+for (int i = 0; i < arr.Length; i++)
+{
+     num[i] = int.Parse(arr[i]);
+     System.Console.Write(num[i]);
+     if (i < arr.Length-1)
+     {
+     System.Console.Write(", ");
+     }
+}
+System.Console.Write("]");
