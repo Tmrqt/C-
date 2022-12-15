01a3737086958436e2e8127d9218889dde54a038
Timerkhan <timerkhan12@gmail.com>
2022-12-15 22:41:37 +0500

    done

---
+++ Task_27/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int[] arr = new int[num];
+int i = 0;
+int sum = 0;
+while (num > 0)
+{
+    arr[i] = num%10;
+    sum = sum + arr[i];
+    num/=10;
+    i++;
+}
+System.Console.WriteLine(sum);
