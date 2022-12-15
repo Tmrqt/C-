2e4d2f23cabcf4473522bd1e25121129c6ac2229
Timerkhan <timerkhan12@gmail.com>
2022-12-15 22:41:26 +0500

    done

---
+++ Task_25/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] Point (string A, string B)
+{
+    int[] num = new int[2];
+    Console.WriteLine($"Введите {A} и {B} через пробел:");
+    string[] arr =  Console.ReadLine().Split();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        num[i] = int.Parse(arr[i]);
+    }
+    return num;
+}
+int[] num1 = Point ("A", "B");
+System.Console.WriteLine((Math.Pow(num1[0], num1[1])));
